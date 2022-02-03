66fa816f3ad1ffdd94614eca31b5dbdd026e9aef
DerpiDitzi <58954591+DerpiDitzi@users.noreply.github.com>
2022-02-03 15:29:25 -0600

    Field updates

--- db/migrate/20220121145724_create_books.rb
+++ db/migrate/20220121145724_create_books.rb
@@ -2,7 +2,9 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
-
+	  t.string :author
+	  t.numeric :price
+	  t.date :published
       t.timestamps
     end
   end
